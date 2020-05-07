33b492a09ec6397885c3498f76765ee8322851b8
chuck francis <chuck_francis10@yahoo.com>
2020-05-07 18:15:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ages = new List<int>();
+
+            ages.Add(23);
+            ages.Add(20);
+            
+            Console.WriteLine();
+            Console.Write("Press enter to shut down...");
+            Console.ReadLine();
+        }
+        private static void DemonstrateTextFileStorage()...
